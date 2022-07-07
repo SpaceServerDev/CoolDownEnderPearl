6b25bae48228150c5cb542dca76e6ee4d0e88ce3
yurisi0212 <Dev.yurisi@gmail.com>
2022-07-07 21:04:22 +0900

    fix error

--- src/space/yurisi/CoolDownEnderPearl/Main.php
+++ src/space/yurisi/CoolDownEnderPearl/Main.php
@@ -5,6 +5,7 @@ namespace space\yurisi\CoolDownEnderPearl;
 
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 use space\yurisi\CoolDownEnderPearl\event\player\ItemUseEvent;
 use space\yurisi\CoolDownEnderPearl\event\player\QuitEvent;
 use space\yurisi\CoolDownEnderPearl\task\SendTask;
@@ -15,9 +16,11 @@ class Main extends PluginBase {
 
     private array $progress = [];
 
+    private int $coolDown_second;
+
     public function setTask(Player $player) {
         $this->progress[$player->getName()] = $player->getXpManager()->getXpProgress();
-        $task = $this->getScheduler()->scheduleRepeatingTask(new SendTask($player, $this), 5);
+        $task = $this->getScheduler()->scheduleRepeatingTask(new SendTask($player, $this), $this->getSecond());
         $this->tasks[$player->getName()] = $task->getTask()->getHandler();
     }
 
@@ -32,13 +35,22 @@ class Main extends PluginBase {
     }
 
     public function removeTask(Player $player) {
+        if(!(isset($this->progress[$player->getName()]) && isset($this->tasks[$player->getName()]))) return;
         $player->getXpManager()->setXpProgress($this->progress[$player->getName()]);
         unset($this->tasks[$player->getName()]);
         unset($this->progress[$player->getName()]);
     }
 
+    public function getSecond(): int{
+        return $this->coolDown_second;
+    }
+
     protected function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents(new ItemUseEvent($this), $this);
         $this->getServer()->getPluginManager()->registerEvents(new QuitEvent($this), $this);
+        $config = new Config($this->getDataFolder().'setting.yml', Config::YAML, [
+            '秒数' => 5
+        ]);
+        $this->coolDown_second = $config->get('秒数');
     }
 }
--- src/space/yurisi/CoolDownEnderPearl/task/SendTask.php
+++ src/space/yurisi/CoolDownEnderPearl/task/SendTask.php
@@ -9,18 +9,18 @@ use space\yurisi\CoolDownEnderPearl\Main;
 
 class SendTask extends Task {
 
-    private const MAX_TICK_COUNT = 20;
     private int $tick_counter = 1;
 
     public function __construct(private Player $player, private Main $main) {
     }
 
     public function onRun(): void {
-        if ($this->tick_counter >= self::MAX_TICK_COUNT) {
+        $max_tick = $this->main->getSecond() * 4;
+        if ($this->tick_counter >= $max_tick) {
             $this->getHandler()->cancel();
             return;
         }
-        $this->player->getXpManager()->setXpProgress($this->tick_counter / self::MAX_TICK_COUNT);
+        $this->player->getXpManager()->setXpProgress($this->tick_counter / $max_tick);
         $this->tick_counter++;
     }
 
